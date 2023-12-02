ddf339c2d82da82a7296b2a158004208e88ca0f6
Alex <50965891+alexanderpham@users.noreply.github.com>
2023-12-02 13:54:40 -0700

    Update memMap.h

--- src/memMap.h
+++ src/memMap.h
@@ -25,6 +25,29 @@ pointer to the mapped memory or NULL in case of errors.
 #include <fcntl.h>
 #include <sys/mman.h>
 
+/*
+
+Function: createFileMap
+
+Purpose: create a memory-mapped file with the specified filename and 
+allocate the specified amount of disk space, returning a pointer to 
+the mapped memory for subsequent read and write operations
+
+Input: takes a string of the file in (pointer to a char)
+
+      - takes in the number of bytes of the file that is to be mapped
+
+Output: pointer to the memory-mapped region created for the file
+
+Details:
+The createFileMap function is designed to facilitate the creation of a memory-mapped file. 
+It opens a file specified by filename with write-only access, creating the file 
+if it does not exist. Subsequently, it allocates disk space for the file using fallocate to
+ensure sufficient space is reserved. Finally, it utilizes mmap to map the file into memory, 
+returning a pointer (char*) to the beginning of the memory-mapped region. The main purpose is
+to provide a convenient interface for creating and accessing memory-mapped files
+
+*/
 char* createFileMap(char* filename, int filesize);
 
 #endif
