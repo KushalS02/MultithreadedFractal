ee370a8313d6835e98ef1eee8eb9a381c5538b08
Alex <50965891+alexanderpham@users.noreply.github.com>
2023-12-02 10:17:27 -0700

    Update memMap.h

--- src/memMap.h
+++ src/memMap.h
@@ -1,3 +1,21 @@
+/*
+
+Authours: Alexander Pham, Kushal Saini and Nimrat Brar
+
+Course: COMP 3659 - 001
+
+Programming Project 2
+
+File name: memMap.h
+
+Instructor: Marc Schroeder
+
+Purpose:  creates a new file with the specified name, 
+allocates a given amount of space for it on disk, and 
+maps the file into memory for writing, returning a 
+pointer to the mapped memory or NULL in case of errors.
+
+*/
 
 #ifndef MEMMAP_H
 #define MEMMAP_H
